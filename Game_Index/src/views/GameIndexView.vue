246c883a00fa1ed63dcd55922185fe4e80cfe595
MadsStaalMedia <msan71643@edu.ucl.dk>
2026-03-24 10:35:48 +0100

    Update GameIndexView.vue

--- Game_Index/src/views/GameIndexView.vue
+++ Game_Index/src/views/GameIndexView.vue
@@ -1,3 +1,46 @@
+<script setup>
+
+const gameList = [
+    {
+        name: "spil 1",
+        desc: "et spil hvor du spiller",
+        publisher: "Spil Firma",
+        date: "5-5-1955",
+        added: "1-1-2026",
+        genre: "spillespil",
+        players: "2-4",
+        age: "+99",
+        complex: "mellem",
+        copies: "4"
+    },
+    {
+        name: "spil 2",
+        desc: "et spil hvor du spiller",
+        publisher: "Spil Firma",
+        date: "5-5-1955",
+        added: "1-1-2026",
+        genre: "spillespil",
+        players: "2-4",
+        age: "+99",
+        complex: "mellem",
+        copies: "4"
+    },
+    {
+        name: "spil 3",
+        desc: "et spil hvor du spiller",
+        publisher: "Spil Firma",
+        date: "5-5-1955",
+        added: "1-1-2026",
+        genre: "spillespil",
+        players: "2-4",
+        age: "+99",
+        complex: "mellem",
+        copies: "4"
+    }
+];
+
+</script>
+
 <template>
   <div class="gameindex">
     <h1>This is a game index page</h1>
