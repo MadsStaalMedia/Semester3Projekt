28d2a72661201c4604975aa56d7895a5378d5bd0
MadsStaalMedia <msan71643@edu.ucl.dk>
2026-03-30 10:02:58 +0200

    Update GameIndexView.vue

--- Game_Index/src/views/GameIndexView.vue
+++ Game_Index/src/views/GameIndexView.vue
@@ -92,7 +92,7 @@ function toggleGameInfo(game) {
   }
 }
 
-const currentPage = 3;
+const gamePage = 3;
 
 const displayedGames = computed(() => {
   return gameList.value.filter((game, index) => index < gamePage)
