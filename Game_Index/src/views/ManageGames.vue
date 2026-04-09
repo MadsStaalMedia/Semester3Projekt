dd0f75619d8be602ff93cedaa3dc5c62155fdb02
MadsStaalMedia <msan71643@edu.ucl.dk>
2026-04-09 10:10:47 +0200

    Update ManageGames.vue

--- Game_Index/src/views/ManageGames.vue
+++ Game_Index/src/views/ManageGames.vue
@@ -2,8 +2,38 @@
 
     import { ref } from 'vue';
 
+    const name = ref('');
+    const desc = ref('');
+    const publisher = ref('');
+    const date = ref('');
+    const genre = ref('');
+    const players = ref('');
+    const age = ref('');
+    const complex = ref('');
+    const copies = ref('');
+    const img = ref()
+
     const onSubmit = () => {
         console.log("submitted");
+
+        const res = fetch('https://svendborgbraetspilliste-default-rtdb.europe-west1.firebasedatabase.app/games.json', {
+            method: 'POST',
+            headers:  {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                name: name.value,
+                desc: desc.value,
+                publisher: publisher.value,
+                date: date.value,
+                added: new Date,
+                genre: genre.value,
+                players: players.value,
+                age: age.value,
+                complex: complex.value,
+                copies: copies.value,
+            }),
+        });
     };
 
 </script>
@@ -12,25 +42,25 @@
 
     <form @submit.prevent="onSubmit">
 
-        Titel: <input /><br>
+        Titel: <input v-model="name" /><br>
 
-        Kort beskrivelse: <input /><br>
+        Kort beskrivelse: <input v-model="desc" /><br>
 
-        Udgiver: <input /><br>
+        Udgiver: <input v-model="publisher" /><br>
 
-        Udgivelsesdato: <input type="date" /><br>
+        Udgivelsesdato: <input type="date" v-model="date" /><br>
 
-        Antal spillere: <input /><br>
+        Antal spillere: <input v-model="players" /><br>
 
-        Anbefalet alder: <input /><br>
+        Anbefalet alder: <input v-model="age" /><br>
         
-        Kompleksitet: <input /><br>
+        Kompleksitet: <input v-model="complex" /><br>
 
-        Genre: <input /><br>
+        Genre: <input v-model="genre" /><br>
 
-        Antal kopier: <input /><br>
+        Antal kopier: <input v-model="copies" /><br>
 
-        Billede: <input /><br>
+        Billede: <input type="file" accept="image/png, image/gif, image/jpeg" /><br>
 
         <button type="submit">Tilføj spil</button>
 
