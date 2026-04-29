135f7cc0e5d93f9d79a1d2ffdd7f92fee320d509
MadsStaalMedia <msan71643@edu.ucl.dk>
2026-04-29 11:20:10 +0200

    Image display

--- Game_Index/src/views/GameIndexView.vue
+++ Game_Index/src/views/GameIndexView.vue
@@ -72,7 +72,7 @@
 
       <select :value="1">
         <option @click="sortAlphabet()" :value="1">Alfabetisk</option>
-        <option @click="sortRelease()" :value="2">Udgivelsesdato</option>
+        <option @click="sortRelease()" :value="2">Udgivelsesår</option>
         <option @click="sortAdded()" :value="3">Sidst Tilføjet</option>
       </select>
 
@@ -80,7 +80,7 @@
 
         <!--<img src="img/catan.jpg" aspect-ratio="1" alt="test">-->
         <div class="game_imgAndTitle" v-on:click="toggleGameInfo(game)">
-          <img class="game_img" src="/img/monopoly_pokemon.webp" />
+          <img v-if="game.imgUrl" :src="game.imgUrl" :alt="game.name" style="max-width: 200px; max-height: 200px; object-fit: contain;" />
           <h3 class="game_title">{{ game.name }}</h3>
           <p class="game_desc">{{ game.desc }}</p>
         </div>
@@ -89,11 +89,9 @@
           <div v-if="activeGame === game" class="game_info">
             <ul class="game_info-text">
               <li>Udgiver: {{ game.publisher }}</li>
-              <li>Udgivelsesdato: {{ game.date }}</li>
+              <li>Udgivelsesår: {{ game.date }}</li>
               <li>Antal spillere: {{ game.players }}</li>
               <li>Anbefalet alder: {{ game.age }}</li>
-              <li>Kompleksitet: {{ game.complex }}</li>
-              <li>Genre: {{ game.genre }}</li>
             </ul>
           </div>
         </transition>
--- Game_Index/src/views/ManageGames.vue
+++ Game_Index/src/views/ManageGames.vue
@@ -1,26 +1,56 @@
 <script setup>
 
     import { ref } from 'vue';
+    import { initializeApp } from 'firebase/app';
+    import { getStorage, ref as storageRef, uploadBytes, getDownloadURL } from 'firebase/storage';
+
+    const firebaseConfig = {
+        apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+        authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+        databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
+        projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+        storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+        messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+        appId: import.meta.env.VITE_FIREBASE_APP_ID,
+
+    };
+    const app = initializeApp(firebaseConfig);
+    const storage = getStorage(app);
 
     const name = ref('');
     const desc = ref('');
     const publisher = ref('');
     const date = ref('');
-    const genre = ref('');
     const players = ref('');
     const age = ref('');
-    const complex = ref('');
     const copies = ref('');
-    const img = ref(null);
+    const imgFile = ref(null);
+    const imgPreview = ref(null);
+
+    function onFileChange(e) {
+        const file = e.target.files[0];
+        if (!file) return;
+        imgFile.value = file;
+        imgPreview.value = URL.createObjectURL(file);
+    }
 
     
 
-    const onSubmit = () => {
-        console.log("submitted");
+    const onSubmit = async () => {
+        let imgUrl = null;
 
-        const res = fetch('https://svenborgbraetspilindex-default-rtdb.europe-west1.firebasedatabase.app/games.json', {
+        if (imgFile.value) {
+            const fileRef = storageRef(storage, `games/${Date.now()}_${imgFile.value.name}`);
+            await uploadBytes(fileRef, imgFile.value);
+            imgUrl = await getDownloadURL(fileRef);
+        } else {
+            alert('Vælg venligst et billede.');
+            return;
+        }
+
+        await fetch('https://svenborgbraetspilindex-default-rtdb.europe-west1.firebasedatabase.app/games.json', {
             method: 'POST',
-            headers:  {
+            headers: {
                 'Content-Type': 'application/json',
             },
             body: JSON.stringify({
@@ -28,13 +58,13 @@
                 desc: desc.value,
                 publisher: publisher.value,
                 date: date.value,
-                added: new Date,
-                genre: genre.value,
+                added: new Date().toString(),
                 players: players.value,
                 age: age.value,
-                complex: complex.value,
                 copies: copies.value,
+                imgUrl,
             }),
+
         });
 
     };
@@ -51,17 +81,17 @@
 
         Udgiver: <input v-model="publisher" /><br>
 
-        Udgivelsesdato: <input type="date" v-model="date" /><br>
+        Udgivelsesår: <input type="number" v-model="date" /><br>
 
         Antal spillere: <input v-model="players" /><br>
 
         Anbefalet alder: <input v-model="age" /><br>
-        
-        Kompleksitet: <input v-model="complex" /><br>
 
-        Genre: <input v-model="genre" /><br>
+        Antal kopier: <input type="number" v-model="copies" /><br>
 
-        Antal kopier: <input v-model="copies" /><br>
+        <input type="file" accept="image/*" required @change="onFileChange" /><br>
+
+        <img v-if="imgPreview" :src="imgPreview" alt="Preview" style="max-width: 200px; margin: 8px 0;" /><br>
 
         <button type="submit">Tilføj spil</button>
 
